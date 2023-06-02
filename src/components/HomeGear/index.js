44c5b33d10369688d97e000d254a12ecf622864c
viet hung <viethung0823@gmail.com>
2023-06-02 23:33:41 +0700

    build

--- src/components/HomeGear/index.js
+++ src/components/HomeGear/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from "react";
+import React, {useState} from "react";
 
 function HomeGear() {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
 		<section className="home-gear">
 			<a href="" title="EG Swag" target="_blank">
-				<div className="home-gear__image">
+				<div className="home-gear__image" className={isHovered ? "home-gear__image active" : "home-gear__image"} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 					<img alt="swag shop" className="pc-only loaded" data-src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" lazy="loaded"></img>
 					<img alt="swag shop" className="sp-only" data-src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" lazy="loading"></img>
 				</div>
