cb219ff1eb24dd3d780a6ef6fe48634afc1e71b5
viet hung <viethung0823@gmail.com>
2023-06-08 16:18:27 +0700

    build

--- src/components/HomeGear/index.js
+++ src/components/HomeGear/index.js
@@ -7,7 +7,7 @@ function HomeGear() {
 
 	return (
 		<section className="home-gear">
-			<a href="" title="EG Swag" target="_blank">
+			<a  title="EG Swag" target="_blank">
 				<div className="home-gear__image" className={isHovered ? "home-gear__image active" : "home-gear__image"} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 					<img alt="swag shop" className="pc-only loaded" data-src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" lazy="loaded"></img>
 					<img alt="swag shop" className="sp-only" data-src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" src="https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/ewShop.png" lazy="loading"></img>
