deb690bda7a762ef2dc4302ef0485ff9694a6f33
viet hung <viethung0823@gmail.com>
2023-06-13 17:06:12 +0700

    build

--- src/App.js
+++ src/App.js
@@ -18,6 +18,19 @@ function App() {
 		console.log("loaded");
 		$("#siteWrapper").appendTo(".app");
 	});
+	document.addEventListener('DOMContentLoaded', function() {
+		if (window.location.hash) {
+			setTimeout(() => {
+				const element = document.querySelector(window.location.hash);
+				if (element) {
+					window.scrollTo({
+						top: element.offsetTop,
+						behavior: 'smooth'
+					});
+				}
+			}, 100)
+		}
+	});
 
 	return (
 		<>
