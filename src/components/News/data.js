c511022fee66977923d7e78ea46d54122d9a4c5c
viet hung <viethung0823@gmail.com>
2023-06-15 09:00:02 +0700

    update

--- src/components/News/data.js
+++ src/components/News/data.js
@@ -6,13 +6,13 @@ export const news = [
 		link: "https://mixmagjapan.com/",
 	},
 	{
-		title: "Market Entry 001 - Discover the opportunity",
+		title: "Enter Japan - Discover the opportunity",
 		date: new Date("June 23, 2023"),
 		img: "https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/news/market.png",
 		link: "https://www.linkedin.com/feed/update/urn:li:activity:7072764717844320256",
 	},
 	{
-		title: "Mixmag Website, Relaunch",
+		title: "BTV, Tokyo Launch",
 		date: new Date("March 23, 2023"),
 		img: "https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/news/beyond.png",
 		link: "https://www.eggworm.jp/beyondthevines",
