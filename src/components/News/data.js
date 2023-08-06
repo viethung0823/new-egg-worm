77f6b90fb9341e0ff865fab290b7fc58f0149b3d
viet hung <viethung0823@gmail.com>
2023-08-06 19:54:05 +0700

    asdasd

--- src/components/News/data.js
+++ src/components/News/data.js
@@ -1,5 +1,11 @@
 export const news = [
 	{
+		title: "Enter Japan 002 - Enter the Right Way",
+		date: new Date("July 23, 2023"),
+		img: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1691125477279-PQ19VY8F1576QNP42W3G/Newsletter+2.jpeg?format=2500w",
+		link: "https://www.eggworm.jp/news/enter-japan-002-enter-the-right-way",
+	},
+	{
 		title: "Mixmag Website, Relaunch",
 		date: new Date("June 23, 2023"),
 		img: "https://raw.githubusercontent.com/kunsansang/new-egg-worm/main/src/assets/news/mixmag.png",
