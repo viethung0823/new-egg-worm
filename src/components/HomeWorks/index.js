3006e0fa4955b5b890a853b4e066215805731dda
viet hung <viethung0823@gmail.com>
2023-09-12 08:30:31 +0700

    update jp text

--- src/components/HomeWorks/index.js
+++ src/components/HomeWorks/index.js
@@ -3,30 +3,6 @@
 import React, {useRef, useEffect, useState} from "react";
 import {useTranslation} from "react-i18next";
 
-const items = [
-	{
-		id: 1,
-		imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/ac09a66e-3604-425e-a1ef-fe2def98bb74/288020857_10159108756540698_9000293709595520415_n.jpeg?format=2500w",
-		title: "BRUICHLADDICH",
-		description: "Bruichladdich were looking to create awareness in 2022 for their product range in the Japanese market. They wanted a launch event in line with their target demographic and PR reach that would assist them in telling their brand story on a national scale.",
-		url: "https://www.eggworm.jp/en/our-work/bruichladdich",
-	},
-	{
-		id: 2,
-		imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596417754268-0HYZGZ8BXFKG20TNHSEK/107051076_2987167684885523_3111168653457907190_n.jpg",
-		title: "TAXI OVATION",
-		description: "To help the hospitals combat Covid-19 with Taxi Ovation – a fundraiser was created to provide free taxi-ride services for healthcare workers in partnership with Makita General Hospital and Kyoritsu Taxi Co., Ltd. The aim was to increase the welfare of hospital staff during this pandemic as well as providing business for companies who were affected by it.",
-		url: "https://www.eggworm.jp/en/our-work/taxi-ovation",
-	},
-	{
-		id: 3,
-		imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596418713811-XAM7LLHGZWOES91AAL3D/28161740_10155415421745698_2751362336498237194_o_10155415421745698.jpg",
-		title: "ONITSUKA TIGER",
-		description: "To mark 69 years of selling sneakers, Onitsuka Tiger opened up a flagship store in the heart of Tokyo. The opening night enhanced the relevance of the brand as one of Japan's original sneaker brands.",
-		url: "https://www.eggworm.jp/en/our-work/onitsuka-tiger",
-	},
-];
-
 function HomeWorks() {
 	const {t, i18n} = useTranslation();
 	const [activeIndex, setActiveIndex] = useState(null);
@@ -52,6 +28,30 @@ function HomeWorks() {
 		};
 	}, []);
 
+	const items = [
+		{
+			id: 1,
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/ac09a66e-3604-425e-a1ef-fe2def98bb74/288020857_10159108756540698_9000293709595520415_n.jpeg?format=2500w",
+			title: "BRUICHLADDICH",
+			description: t("WOKR.1"),
+			url: "https://www.eggworm.jp/en/our-work/bruichladdich",
+		},
+		{
+			id: 2,
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596417754268-0HYZGZ8BXFKG20TNHSEK/107051076_2987167684885523_3111168653457907190_n.jpg",
+			title: "TAXI OVATION",
+			description: t("WOKR.2"),
+			url: "https://www.eggworm.jp/en/our-work/taxi-ovation",
+		},
+		{
+			id: 3,
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596418713811-XAM7LLHGZWOES91AAL3D/28161740_10155415421745698_2751362336498237194_o_10155415421745698.jpg",
+			title: "ONITSUKA TIGER",
+			description: t("WOKR.3"),
+			url: "https://www.eggworm.jp/en/our-work/onitsuka-tiger",
+		},
+	];
+
 	return (
 		<section className="home-works" id="our-work">
 			<header className="home-works-header">
@@ -71,7 +71,7 @@ function HomeWorks() {
 								<h3 className="home-works-info__title">{item.title}</h3>
 								<p className="home-works-info__text">{item.description}</p>{" "}
 								<a href={item.url} className="home-works-link arrow-button active">
-									<span className="text">View Details</span> <span className="arrow"></span> <span className="line"></span>
+									<span className="text">{t("WOKR.detail")}</span> <span className="arrow"></span> <span className="line"></span>
 								</a>
 							</div>
 							<div className="cover"></div>
@@ -81,7 +81,7 @@ function HomeWorks() {
 			</div>
 			<footer className="home-works-footer">
 				<a href={i18n.language === "jp" ? "https://www.eggworm.jp/jp/our-work" :"https://www.eggworm.jp/en/our-work"} className="home-works-footer__link">
-					View All<span></span>
+				{t("WOKR.all")}<span></span>
 				</a>
 			</footer>
 		</section>
