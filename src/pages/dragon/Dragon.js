7180df656583c5af37e4529892c44a8ec343d5db
Viet Hung <viethung0823@gmail.com>
2024-01-21 13:12:59 +0700

    dragon page

---
+++ src/pages/dragon/Dragon.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./index.css";
+
+function DragonPage() {
+	return (
+		<div className="dragonPage">
+			<div className="blockText block-1">
+				<h2 className="title">Looking Back, Looking Ahead.</h2>
+				<p className="byEggorm">by Eggworm</p>
+				<div className="divider"></div>
+				<p className="asWe ">
+					As we leap into the Year of the Dragon, we're not just turning a page; we're starting a whole new chapter.  This past year, thanks to our extraordinary partners, we marked a series of groundbreaking firsts for Japan that will propel further advancement.  This year, we look forward to continuing this momentum across Asia, supporting our partners' development and
+					exploring new opportunities.
+					<br />
+					<br />A bright future awaits as events marketing and experiential realms merge, giving rise to a unique fusion of technology, real-world engagement, and the seamless intertwining of brands, companies, and cultural experiences.
+				</p>
+				<p className="jpText mt20">辰年を迎え、エッグワームはアジア市場での新しい展開を始めます。 私たちのイベントマーケティングと体験型分野の融合は、単に市場を変革するだけでなく、技術と文化の境界を再定義します。</p>
+			</div>
+			<div className="dragon2024"></div>
+			<div className="blockText block-2">
+				<p className="asWe">Turning great ideas into 'Wow, did we just do that?' moments. We love exceeding expectations.</p>
+				<p className="jpText">素晴らしいアイデアを驚きの瞬間へと変えること、期待を超えることが私たちの喜びです。</p>
+			</div>
+			<div className="gallaryStack gallary1"></div>
+			<div className="blockText block-2">
+				<p className="asWe">For us balance is important with our partners and within our own team. The power of that balance lies in the clarity of it.  Less suits, more superheroes.</p>
+				<p className="jpText">この新たな時代の門出に、私たちは共に前例のない成果を目指します。</p>
+			</div>
+			<div className="gallaryStack gallary2"></div>
+			<div className="blockText block-2">
+				<p className="asWe">Transitioning from an atypical beginning, my unwavering commitment has been to approach both life and my role as CEO in an uncommon manner.</p>
+				<p className="jpText">挑戦を成長の機会に変えることで、共に素晴らしい未来を築きましょう！</p>
+				<p>- Masaki Kawamura, CEO</p>
+			</div>
+			<div className="gallaryStack gallary3"></div>
+			<div className="blockText block-2">
+				<p className="asWe">We are problem solvers.  We are storytellers.</p>
+				<p className="jpText">共に成長し、新しい物語を紡ぎ出しましょう。</p>
+			</div>
+			<div className="gallaryStack gallary4"></div>
+			<div className="dragonPageFooter">
+				<div className="eggwormlogo"></div>
+				<div className="footerRight">
+					<p className="footerTitle"> We are from TOKYO.</p>
+					<p className="footerSub">東京から世界へ。</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default DragonPage;
